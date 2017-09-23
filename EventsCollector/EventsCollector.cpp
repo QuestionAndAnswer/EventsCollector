92db4a9a74c96bb124b0c1227b9b177b7411b95f
QuestionAndAnswer <forbiddentool@gmail.com>
2017-09-23 09:47:16 +0300

    Initial

---
+++ EventsCollector/EventsCollector.cpp
@@ -0,0 +1,14 @@
+// EventsCollector.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "EventsCollector.h"
+
+//LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+//
+//}
+
+int testCall(int a, int b)
+{
+	return a + b;
+}
